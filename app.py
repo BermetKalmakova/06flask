4a9dd17ba7319a69ed1caaec750bd2d08e3ef345
GordonLei <glei@stuy.edu>
2018-02-26 23:27:13 -0500

    pushed files?

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 import os, sqlite3, hashlib, json, requests, sys
 import pymongo, json
+from movies import queryTitle, queryDirector, queryGenre, queryYear
 
 app= Flask(__name__)
 app.secret_key = os.urandom(64)
@@ -16,7 +17,27 @@ def root():
     db = connection.leiGkalmakovaB
     collection = db.movies
     collection.insert_many(moviesList) 
-    return render_template("form.html")
+    ## I dont know why request.form['title'] etc. is giving me a bad request so I used request.form.get('title') etc. instead
+    boolean = request.form.get('title') != "" or request.form.get('genre') != "" or request.form.get('year') != "" or request.form.get('director') != ""
+
+    ## I dont know why request.form['title'] etc. is giving me a bad request so I used request.form.get('title') etc. instead
+    if ((request.method == 'POST' or  request.method == 'GET') and boolean):
+        if (request.form.get('title') != ""):
+            querySearch = request.form.get('title')
+            return render_template('form.html', info = queryTitle(querySearch))
+        if (request.form.get('genre') != ""):
+            querySearch = request.form.get('genre')
+            return render_template('form.html', info = queryGenre(querySearch))
+        if (request.form.get('year') != ""):
+            querySearch = request.form.get('year')
+            return render_template('form.html', info = queryYear(querySearch))
+        if (request.form.get('director') != ""):
+            querySearch = request.form.get('director')
+            return render_template('form.html', info = queryDirector(querySearch))
+    else:
+        return render_template('form.html')
+    ## return render_template("form.html")
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
---
+++ movies.py
@@ -0,0 +1,51 @@
+
+## download link: https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json
+##  American Movies Scrapped from America 
+
+## JSON contains: title, year, director,cast, genre, and notes
+
+##
+## We imported by importing the json module
+##  Then we open the json and used a variable to hold the json contents (this variable is a list)
+##  Using insert_many we inserted everything in the list into the database. 
+##
+##
+
+import pymongo, json
+with open("movies.json", "r") as f:
+    moviesList = json.load(f)
+
+connection = pymongo.MongoClient("homer.stuy.edu")
+db = connection.leiGkalmakovaB 
+collection = db.movies
+collection.insert_many(moviesList) 
+
+
+def queryTitle(title):
+    queryList = collection.find({"title": title})
+    return queryList
+
+def queryYear(year):
+    queryList = collection.find({"year": year})
+    return queryList
+
+def queryDirector(director):
+    queryList = collection.find({"director": director})
+    return queryList
+
+def queryGenre(genre):
+    queryList = collection.find({"genre": genre})
+    return queryList
+
+##def queryInclusiveBetweenYears(y1,y2):
+##    queryList = []
+##    for each in collection.find({"$and": [{"year":{"$lte": y2}}, {"year":{"$gte": y1}}]}):
+##        #print each
+##        queryList.extend(each)
+##    return queryList
+
+##queryTitle("After Dark in Central")
+##queryYear(1900)
+##queryDirector("James H. White")
+##queryGenre("Short")
+##queryInclusiveBetweenYears(1900,1902)
